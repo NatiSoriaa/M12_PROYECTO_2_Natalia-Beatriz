ab414af640937d21558a7c00d13f5e08e6e650d2
NatiSoriaa <naticsoria@gmail.com>
2024-10-03 16:12:12 +0200

    index

--- PROYECTO_API.py
+++ PROYECTO_API.py
@@ -1,2 +1,15 @@
 from flask import Flask,render_template,request,redirect,url_for, session, flash
 import mysql.connector as mysql
+
+app = Flask(__name__)
+
+conexion1=mysql.connector.connect(host="localhost", user="root", passwd="")
+cursor1=conexion1.cursor()
+cursor1.execute("show databases")
+for base in cursor1:
+    print(base)
+
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
