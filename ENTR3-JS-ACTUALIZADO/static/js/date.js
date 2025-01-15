19eecdc0192c572daae318d8d98bf0b1b249d649
beatrizabadj <145197915+beatrizabadj@users.noreply.github.com>
2025-01-15 23:52:12 +0100

    date.js

---
+++ ENTR3-JS-ACTUALIZADO/static/js/date.js
@@ -0,0 +1,14 @@
+function mostrarHoraActual() {
+    const fecha = new Date();
+    const horas = fecha.getHours().toString().padStart(2, '0');
+    const minutos = fecha.getMinutes().toString().padStart(2, '0');
+
+    const horaActual = `${horas}:${minutos}`;
+    document.getElementById('currentTime').textContent = `TIME: ${horaActual} `;
+}
+
+//se actualiza la hora
+setInterval(mostrarHoraActual, 1000);
+
+// Mostrar la hora inmediatamente al cargar la página
+mostrarHoraActual();
